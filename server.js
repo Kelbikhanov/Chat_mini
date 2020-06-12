3969b3e275f3a56a0caa5d3d5d94552f44120366
kelbikhanov <rusik9091@gmail.com>
2020-06-12 12:50:59 +0300

    server2

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require('express');        /*Подключили экспресс */
 
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const app = express();          /*Сщздали экспресс приложение */
+const server = require('http').Server(app);             /*Создали http server */
+const io = require('socket.io')(server);               /*Создали сокеты */
+
+app.use(express.json());
 
 const rooms = new Map();
 
 app.get('/rooms', (req, res) => {
   res.json(rooms);
+});
+
+app.post('/rooms', (req, res) => {
+  const {roomId, userName} = req.body;
+  if (!rooms.has(roomId)) {
+    rooms.set(roomId,
+       new Map([
+      ['users', new Map()],
+      ['messages', []],
+    ]));
+  }
+  res.send();
 });
 
 io.on('connection', (socket) => {
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import socket from '../socket';
+import axios from 'axios';
 
 function JoinBlock() {
+
+  const [roomId, setRoomId] = React.useState('');
+  const [userName, setUserName] = React.useState('');
+
+  const onEnter = () => {
+    if( !roomId || !userName) {
+      return alert('Неверные данные');
+    }
+    axios.post('/rooms')
+  }
+
   return (
     <div className="join-block">
-      <input type="text" placeholder="Room ID" />
-      <input type="text" placeholder="Ваше имя" />
-      <button className="btn btn-success">ВОЙТИ</button>
+
+      <input 
+        type="text"
+        placeholder="Room ID"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)} 
+        />
+
+      <input
+        type="text"
+        placeholder="Ваше имя"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)} 
+        />
+        
+      <button onClick={onEnter} className="btn btn-success">ВОЙТИ</button>
     </div>
   );
 }
